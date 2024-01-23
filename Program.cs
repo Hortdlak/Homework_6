5933d6b4333d1473cc3784b472b97aa6546a69ac
Hortdlak <shegarov_sergei@mail.ru>
2024-01-24 03:20:09 +0700

    Mandatory tasks completed

--- Program.cs
+++ Program.cs
@@ -10,18 +10,50 @@ namespace Homework_6
     {
         static void Main(string[] args)
         {
-            #region
+            #region Строка из матрицы символов
             //Задайте двумерный массив символов(тип char[,]). Создать строку из символов этого массива.
 
-            string result = CreatingStringForCharMatrix();
+            //string result = CreatingStringForCharMatrix();
 
-            Console.Write(result);
+            //Console.Write(result);
+
+            #endregion
+
+            #region Изменение регистра с помощью "ToLower"
+
+            //Задайте строку, содержащую латинские буквы в обоих регистрах.
+            //Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+            //string text = "Надо изменить регистр. В этом задании.";
+            //string result = text.ToLower();
+            //Console.WriteLine(result);
+
+            #endregion
+
+            #region
+
+            //Задайте произвольную строку.Выясните, является ли она палиндромом.
+
+            string testWord = "А роза упала на лапу Азора";
+
+            string answer = isPalindrome(testWord) ? "Да" : "Нет";
+
+            Console.WriteLine(answer);
 
             #endregion
 
 
 
             Console.ReadKey();
+        }
+
+        private static bool isPalindrome(string testWord)
+        {
+
+            string normalized = new string(testWord.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+
+            return normalized.SequenceEqual(normalized.Reverse());
+
         }
 
         private static string CreatingStringForCharMatrix()
